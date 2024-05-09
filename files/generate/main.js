8b3d709c01d668b2cd171b66190818814115895e
alec nikolas reiter (they/them) <alec@tonic.ai>
2024-05-09 11:22:35 -0400

    Optionally set mode on file

--- files/generate/main.js
+++ files/generate/main.js
@@ -8,6 +8,8 @@ const filepath = path.resolve(path.normalize(rawPath));
 const append = (process.env.INPUT_APPEND || "").toLowerCase() === "true";
 const flags = append && "a" || "w";
 
+const mode = (process.env.INPUT_MODE || "");
+
 if (process.env.STATE_filepath !== undefined) {
   if (process.env.INPUT_CLEANUP !== undefined && process.env.INPUT_CLEANUP.toLowerCase() === "true") {
     console.log(`Removing ${process.env.STATE_filepath}`);
@@ -15,6 +17,11 @@ if (process.env.STATE_filepath !== undefined) {
   }
 } else {
   fs.writeFileSync(filepath, process.env.INPUT_CONTENT, {flags});
+
+  if (mode !== "") {
+    fs.chmodSync(filepath, mode);
+  }
+
   fs.appendFileSync(process.env.GITHUB_STATE, `filepath=${filepath}${EOL}`);
   fs.appendFileSync(process.env.GITHUB_OUTPUT, `filepath=${filepath}${EOL}`);
 }
