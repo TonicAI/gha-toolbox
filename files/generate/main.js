84badc1d8ccc630b3c4e939a84767c2470189b51
alec reiter (they/them) <alec@tonic.ai>
2024-05-09 10:26:12 -0400

    Add option to append to file as well (#17)

--- files/generate/main.js
+++ files/generate/main.js
@@ -2,14 +2,19 @@ const fs = require("fs");
 const path = require("path");
 const {EOL} = require("os");
 
+const rawPath = process.env.INPUT_FILEPATH
+const filepath = path.resolve(path.normalize(rawPath));
+
+const append = (process.env.INPUT_APPEND || "").toLowerCase() === "true";
+const flags = append && "a" || "w";
+
 if (process.env.STATE_filepath !== undefined) {
   if (process.env.INPUT_CLEANUP !== undefined && process.env.INPUT_CLEANUP.toLowerCase() === "true") {
     console.log(`Removing ${process.env.STATE_filepath}`);
     fs.unlinkSync(process.env.STATE_filepath);
   }
 } else {
-  const writePath = path.resolve(process.env.INPUT_FILEPATH);
-  fs.writeFileSync(writePath, process.env.INPUT_CONTENT);
-  fs.appendFileSync(process.env.GITHUB_STATE, `filepath=${writePath}${EOL}`);
-  fs.appendFileSync(process.env.GITHUB_OUTPUT, `filepath=${writePath}${EOL}`);
+  fs.writeFileSync(filepath, process.env.INPUT_CONTENT, {flags});
+  fs.appendFileSync(process.env.GITHUB_STATE, `filepath=${filepath}${EOL}`);
+  fs.appendFileSync(process.env.GITHUB_OUTPUT, `filepath=${filepath}${EOL}`);
 }
