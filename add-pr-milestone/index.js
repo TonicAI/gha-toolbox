298ffa026af17cd8d7964a02fc3a3ea30812ed95
Andrei Andronic <andrei@tonic.ai>
2024-07-26 11:35:29 -0400

    add pr milstone action

---
+++ add-pr-milestone/index.js
@@ -0,0 +1,49 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+    const token = core.getInput("repo-token", { required: true });
+
+    const pullRequest = github.context.payload.pull_request;
+    if (!pullRequest) {
+        console.log("Could not get pull_request from context, exiting");
+        return;
+    }
+
+    const { milestone, number: prNumber } = pullRequest;
+
+    if (milestone) {
+        console.log("Milestone already exists, exiting");
+        return;
+    }
+
+    const client = new github.getOctokit(token);
+
+    const milestones = await client.rest.issues.listMilestones({
+        repo: github.context.payload.repository.name,
+        owner: github.context.payload.repository.owner.login,
+        state: "open",
+    });
+
+    if (milestones.data.length) {
+        const milestoneToAdd =
+            milestones.data.sort()[milestones.data.length - 1];
+        const milestoneNumber = milestoneToAdd.number;
+        await client.rest.issues.update({
+            repo: github.context.payload.repository.name,
+            owner: github.context.payload.repository.owner.login,
+            issue_number: prNumber,
+            milestone: milestoneNumber,
+        });
+        core.setOutput("milestone", milestoneToAdd.title);
+    } else {
+        console.log("No valid milestones found");
+    }
+}
+
+run().catch((error) => {
+    core.setFailed(error.message);
+    if (error instanceof Error && error.stack) {
+        core.debug(error.stack);
+    }
+});
