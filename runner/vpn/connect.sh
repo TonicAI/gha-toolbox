833d386b64cda579e342169755012c9127eb8144
alec nikolas reiter (they/them) <alec@tonic.ai>
2024-07-31 14:46:43 -0400

    Add option to explicitly route to tun0

--- runner/vpn/connect.sh
+++ runner/vpn/connect.sh
@@ -3,6 +3,7 @@ set -e
 
 PING_TARGET="${INPUT_PING_TARGET}"
 DNS_SERVERS="${INPUT_DNS_SERVERS}"
+EXPLICIT_ROUTES="${INPUT_EXPLICIT_ROUTES}"
 
 sudo apt-get update
 sudo apt-get --assume-yes --no-install-recommends install openvpn
@@ -15,8 +16,17 @@ chmod 600 dev.pem
 
 sudo openvpn --config "github_action.ovpn" --log "vpn.log" --daemon
 
+if [ ! -z "${EXPLICIT_ROUTES}" ]; then
+  echo "Updating routing tables"
+  for route in ${EXPLICIT_ROUTES}; do
+    sudo ip route add "${route}" dev tun0
+  done
+  echo "Configured table"
+  ip route
+fi
 
 if [ ! -z "${DNS_SERVERS}" ]; then
+    echo "Configuring DNS to use ${DNS_SERVERS}"
     sudo sed -i "s/#DNS=/DNS=${DNS_SERVERS}/g" /etc/systemd/resolved.conf
     sudo systemctl daemon-reload
     sudo systemctl restart systemd-networkd
@@ -24,5 +34,7 @@ if [ ! -z "${DNS_SERVERS}" ]; then
 fi
 
 if [ ! -z "${PING_TARGET}" ]; then
+  echo "Waiting to make connection to ping target..."
   until ping -c1 "${PING_TARGET}"; do sleep 2; done
+  echo "Contact made"
 fi
