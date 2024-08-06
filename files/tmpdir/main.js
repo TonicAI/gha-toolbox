d916950d004637ec8d8bddc957fe8ada6d8a1116
alec nikolas reiter (they/them) <alec@tonic.ai>
2024-08-06 14:25:19 -0400

    Add files/tmpdir action

---
+++ files/tmpdir/main.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const {EOL} = require("os");
+
+if (process.env.STATE_filepath !== undefined) {
+    fs.rmSync(process.env.STATE_filepath, { recursive: true, force: true })
+} else {
+  const tmpdir = `${path.join(process.env.INPUT_PATH, process.env.INPUT_NAME)}${path.sep}`;
+  fs.mkdtempSync(tmpdir);
+  fs.appendFileSync(process.env.GITHUB_STATE, `tmpdir=${tmpdir}${EOL}`);
+  fs.appendFileSync(process.env.GITHUB_OUTPUT, `tmpdir=${tmpdir}${EOL}`);
+}
