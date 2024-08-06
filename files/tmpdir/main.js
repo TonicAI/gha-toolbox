d557418a730e206ca016c32b90b2aa6a0aad3922
alec nikolas reiter (they/them) <alec@tonic.ai>
2024-08-06 17:59:29 -0400

    make the dir before we make the dir

--- files/tmpdir/main.js
+++ files/tmpdir/main.js
@@ -6,6 +6,7 @@ if (process.env.STATE_filepath !== undefined) {
     fs.rmSync(process.env.STATE_filepath, { recursive: true, force: true })
 } else {
   const tmpdir = `${path.join(process.env.INPUT_PATH, process.env.INPUT_NAME)}${path.sep}`;
+  fs.mkdirSync(tmpdir, {recursive: true});
   fs.mkdtempSync(tmpdir);
   fs.appendFileSync(process.env.GITHUB_STATE, `tmpdir=${tmpdir}${EOL}`);
   fs.appendFileSync(process.env.GITHUB_OUTPUT, `tmpdir=${tmpdir}${EOL}`);
