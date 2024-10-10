6ccfa8dc526bc2553f5d4dc06c06d419e2a317c2
alec nikolas reiter (they/them) <alec@tonic.ai>
2024-10-10 10:25:05 -0400

    Return created dir, not base dir

--- files/tmpdir/main.js
+++ files/tmpdir/main.js
@@ -5,9 +5,9 @@ const {EOL} = require("os");
 if (process.env.STATE_filepath !== undefined) {
     fs.rmSync(process.env.STATE_filepath, { recursive: true, force: true })
 } else {
-  const tmpdir = `${path.join(process.env.INPUT_PATH, process.env.INPUT_NAME)}${path.sep}`;
-  fs.mkdirSync(tmpdir, {recursive: true});
-  fs.mkdtempSync(tmpdir);
+  const tmpdirBase = `${path.join(process.env.INPUT_PATH, process.env.INPUT_NAME)}${path.sep}`;
+  fs.mkdirSync(tmpdirBase, {recursive: true});
+  let tmpdir = fs.mkdtempSync(tmpdirBase);
   fs.appendFileSync(process.env.GITHUB_STATE, `tmpdir=${tmpdir}${EOL}`);
   fs.appendFileSync(process.env.GITHUB_OUTPUT, `tmpdir=${tmpdir}${EOL}`);
 }
