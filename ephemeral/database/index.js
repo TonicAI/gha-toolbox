f32503a01ba11ea7e66878349d268ccf57e033f2
alec nikolas reiter (they/them) <alec@tonic.ai>
2024-12-10 14:42:00 -0500

    Attach full JSON response to ephemeral action

--- ephemeral/database/index.js
+++ ephemeral/database/index.js
@@ -101,6 +101,7 @@ async function runCreate(client, name) {
   database = await client.waitForStart(database);
   const connection = connectionInformation(database);
 
+  core.setOutput("json", JSON.stringify(database));
   core.setOutput("host", connection.host);
   core.setOutput("port", connection.port);
   core.setOutput("username", connection.username);
