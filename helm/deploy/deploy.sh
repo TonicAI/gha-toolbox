8805a391e7e1cab622719c471f0a3a72b09684f0
alec reiter (they/them) <alec@tonic.ai>
2025-08-13 15:22:21 -0400

    Support --dry-run and --debug in helm/deploy (#72)

--- helm/deploy/deploy.sh
+++ helm/deploy/deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 CI_DEBUG=${CI_DEBUG:-}
-if [ ! -z "${CI_DEBUG}" ]; then
-    set -x
+DEBUG=${DEBUG:-}
+
+if [ -n "${CI_DEBUG}" ]; then
+  DEBUG="true"
+fi
+
+if [ "${DEBUG,,}" != "true" ]; then
+  DEBUG=""
 fi
 
 set -eu
@@ -10,24 +16,33 @@ ACTION="create"
 HELM_ARGS=""
 COMMENT_PATTERN="^ *#"
 NAMESPACE="${NAMESPACE,,}"
+DRY_RUN="${DRY_RUN:-}"
 
 function add_helm_args() {
   HELM_ARGS="${HELM_ARGS} ${@}"
 }
 
+if [ -n "${DEBUG}" ]; then
+  add_helm_args --debug
+fi
+
+if [[ "${DRY_RUN,,}" == "true" ]]; then
+  add_helm_args --dry-run
+fi
+
 if [ -f "kustomization.yaml" ]; then
   add_helm_args --post-renderer "${ACTION_PATH}/kustomize.sh"
 fi
 
-if [ ! -z "${HELM_TIMEOUT}" ]; then
+if [ -n "${HELM_TIMEOUT}" ]; then
   add_helm_args --wait --timeout "${HELM_TIMEOUT}"
 fi
 
-if [ ! -z "${CHART_VERSION}" ]; then
+if [ -n "${CHART_VERSION}" ]; then
   add_helm_args --version "${CHART_VERSION}"
 fi
 
-if [ ! -z "${VALUES_FILES}" ]; then
+if [ -n "${VALUES_FILES}" ]; then
   while IFS= read -r line || [[ -n $line ]]; do
     if [ -z "${line}" ] || [[ $line =~ $COMMENT_PATTERN ]]; then
       continue
@@ -42,7 +57,7 @@ set +e
 STATUS=$(helm status -n "${NAMESPACE}" "${NAME}" -o json 2>/dev/null | jq -r '.info.status')
 set -e
 
-if [ ! -z "${REMOVE_FAILED}" ]; then
+if [[ "${REMOVE_FAILED,,}"  == "true" ]]; then
   case "${STATUS}" in
     pending-upgrade )
       helm rollback -n "${NAMESPACE}" "${NAME}" --wait
@@ -53,20 +68,18 @@ if [ ! -z "${REMOVE_FAILED}" ]; then
   esac
 fi
 
-if [ ! -z "${STATUS}" ]; then
+if [ -n "${STATUS}" ]; then
   ACTION="upgrade"
 fi
 
 echo "action=${ACTION}" >> $GITHUB_OUTPUT
 
 set +e
+set -x
 helm upgrade --install "${NAME}" "${CHART}" ${HELM_ARGS}
 EC=$?
+set +x
 set -e
-
-if [ ! -z "${CI_DEBUG}" ]; then
-    ls -al .
-fi
 
 if [ -f "full-manifest.yaml" ]; then
   echo "full-manifest=$(readlink -e full-manifest.yaml)" >> $GITHUB_OUTPUT
