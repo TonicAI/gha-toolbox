cf2178280c50b5d1351676bbdebcddec1fdf6037
Craig Alexander <craig@tonic.ai>
2025-09-26 12:26:26 -0400

    Get release number from release object when ref is null 4 (#83)

--- close-milestone/index.js
+++ close-milestone/index.js
@@ -4,7 +4,7 @@ const github = require("@actions/github");
 async function run() {
     const token = core.getInput("repo-token", { required: true });
     core.debug("Github Payload: " + JSON.stringify(github));
-    const releaseNumberText = github.context.ref ? github.context.ref.substr(11, github.context.ref.length - 1) : github.context.payload.release.name.substr(1, github.context.payload.release.name - 1);
+    const releaseNumberText = github.context.ref ? github.context.ref.substr(11, github.context.ref.length - 1) : github.context.payload.release.name.substring(1);
     const releaseNumber = Number(releaseNumberText);
     if (isNaN(releaseNumber)) return;
     const release = releaseNumber.toString().padStart(3, "0");
